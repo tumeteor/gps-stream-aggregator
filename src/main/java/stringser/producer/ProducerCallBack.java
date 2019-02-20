1fb88c808922596e6751d678a7d9f3402b2806ce
NGUYTU3 <tu.nguyen@daimler.com>
2019-02-20 10:20:19 +0100

    small refactoring for kafka producer

---
+++ src/main/java/stringser/producer/ProducerCallBack.java
@@ -0,0 +1,45 @@
+package stringser.producer;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerCallBack implements Callback {
+
+    private long startTime;
+    private Object key;
+    private String message;
+    private static Logger log = LoggerFactory.getLogger("DemoCallBack");
+
+    public ProducerCallBack(long startTime, Object key, String message) {
+        this.startTime = startTime;
+        this.key = key;
+        this.message = message;
+    }
+
+    /**
+     * A callback method the user can implement to provide asynchronous handling
+     * of request completion. This method will be called when the record sent to
+     * the server has been acknowledged. Exactly one of the arguments will be
+     * non-null.
+     *
+     * @param metadata
+     *            The metadata for the record that was sent (i.e. the partition
+     *            and offset). Null if an error occurred.
+     * @param exception
+     *            The exception thrown during processing of this record. Null if
+     *            no error occurred.
+     */
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        if (metadata != null) {
+            log.info("message(" + key + ", " + message
+                    + ") sent to partition(" + metadata.partition() + "), "
+                    + "offset(" + metadata.offset() + ") in " + elapsedTime
+                    + " ms");
+        } else {
+            exception.printStackTrace();
+        }
+    }
+}
